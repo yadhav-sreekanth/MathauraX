eb66e6c0f35c33eb170bf2daa6a36be343e06f18
yadhav-sreekanth <yadhavvsreelakam@gmail.com>
2025-06-14 14:35:26 +0530

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// Global functionality can be added here
+document.addEventListener('DOMContentLoaded', function() {
+    // Any global initialization code
+    console.log('MathauraX initialized');
+});
